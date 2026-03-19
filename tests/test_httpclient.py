23223557f442dc1f2dad0dbdbdae596dd78e1002
BytemanD <byteman.d@outlook.com>
2026-03-19 21:30:04 +0800

    fix: 修复httpclient测试用例
    Signed-off-by: BytemanD <byteman.d@outlook.com>

--- tests/test_httpclient.py
+++ tests/test_httpclient.py
@@ -1,16 +1,31 @@
+import threading
+
 import httpx
 import pytest
 
 from pystonic.utils import httpclient
 
 
+def start_http_server():
+    import os
+
+    os.system("python -m http.server 80")
+
+
+thread = threading.Thread(target=start_http_server)
+thread.daemon = True
+thread.start()
+
+
 def test_httpclient():
-    client = httpclient.default_client(base_url="https://www.baidu.com/")
-    resp = client.get("https://www.baidu.com/")
+    client = httpclient.default_client(base_url="http://127.0.0.1")
+    resp = client.get("/")
     assert not resp.is_error
 
 
 def test_httpclient_with_check_status():
-    client = httpclient.default_client(timeout=10, raise_for_status=True)
+    client = httpclient.default_client(
+        base_url="http://127.0.0.1", timeout=10, raise_for_status=True
+    )
     with pytest.raises(httpx.HTTPStatusError):
-        client.post("https://www.baidu.com/")
+        client.post("/")
